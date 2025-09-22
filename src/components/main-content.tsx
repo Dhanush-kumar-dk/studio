63fe9855c7c5a1f67b0503983c592697befedfe4
Dhanush kumar K <dhanushkumark62@gmail.com>
2025-09-22 02:54:04 +0000

    need contact us section only in home page and article  page and no need

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -5,7 +5,7 @@ import Contact from './contact';
 
 export default function MainContent({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
-    const showContactForm = pathname !== '/create-post' && !pathname.startsWith('/edit-post');
+    const showContactForm = pathname === '/' || pathname.startsWith('/articles/');
 
     return (
         <>
