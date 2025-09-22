240aa424509810495849635776ef5a93e741e1e2
Dhanush kumar K <dhanushkumark62@gmail.com>
2025-09-22 01:54:07 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCxH8yp7vBAKRcHR6deq0pF5m4o0uQdlxI",
+  authDomain: "studio-5927375734-5754c.firebaseapp.com",
+  projectId: "studio-5927375734-5754c",
+  storageBucket: "studio-5927375734-5754c.appspot.com",
+  messagingSenderId: "228039712999",
+  appId: "1:228039712999:web:b2138907583c7f7c78c242",
+  measurementId: ""
 };
 
 // Initialize Firebase
