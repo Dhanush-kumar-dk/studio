65793249b4393eb81c476b4f586f288d4a85a74a
Dhanush kumar K <dhanushkumark62@gmail.com>
2025-09-22 07:19:08 +0000

    remove add new user

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -13,7 +13,6 @@ export default function ProfilePage() {
           </div>
           <h1 className="text-2xl font-bold">Users</h1>
         </div>
-        <Button>Add New User</Button>
       </div>
       <ProfileForm />
     </div>
