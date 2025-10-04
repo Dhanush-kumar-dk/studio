1215af553a27e1ce5aad61d40b6dd8f6de3bf94a
Dhanush kumar K <dhanushkumark62@gmail.com>
2025-10-04 08:10:29 +0000

    make this as seperate rounded container

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,40 +1,26 @@
 export default function Logo() {
   return (
-    <svg
-      width="32"
-      height="32"
-      viewBox="0 0 100 100"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-8 w-8"
-      aria-label="Debt & Dominion Logo"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M25.75 32.75H43.25V42.5H35.5V67.25H25.75V32.75Z"
-        fill="#F7691A"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M52.5 32.75H70V42.5H62.25V67.25H52.5V32.75Z"
-        fill="#F7691A"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M43.25 32.75H48.125V28H20.875V72.25H48.125V42.5H43.25V32.75ZM25.75 67.25V32.75H35.5V67.25H25.75Z"
-        fill="currentColor"
-        className="text-primary"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M70 32.75H74.875V28H47.625V72.25H74.875V42.5H70V32.75ZM52.5 67.25V32.75H62.25V67.25H52.5Z"
-        fill="currentColor"
-        className="text-primary"
-      />
-    </svg>
+    <div className="flex h-8 w-8 items-center justify-center rounded-md bg-[hsl(213,81%,11%)] p-1">
+      <svg
+        viewBox="0 0 100 100"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-full w-full"
+        aria-label="Debt & Dominion Logo"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M25.75 32.75H43.25V42.5H35.5V67.25H25.75V32.75Z"
+          fill="#F7691A"
+        />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M52.5 32.75H70V42.5H62.25V67.25H52.5V32.75Z"
+          fill="#F7691A"
+        />
+      </svg>
+    </div>
   );
 }
